f7ac1200a0645ed727fd293288e4e24a2bef3126
xiguan-wuge <1584077483@qq.com>
2023-08-07 17:22:35 +0800

    init

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const path = require('path')
+
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify:true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname)
+    }
+    return config
+  }
+}
+
+module.exports = nextConfig
